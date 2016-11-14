dff2a1fb90a6c6ad2a3d8e4198946c28c79a381f
Ais93 <aismcauley00@gmail.com>
2016-11-14 03:28:50 +0000

    layout and modal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  
   def profile
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     
     root 'pages#home'
     get '/profile', to: 'users#profile'
-    
+    resources :users
 
 
  
