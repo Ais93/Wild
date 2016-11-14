a09f5778da514e5835fb715e7737ec60aa9bd723
Ais93 <aismcauley00@gmail.com>
2016-11-14 17:59:40 +0000

    Desgin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,20 @@ class UsersController < ApplicationController
     end
   end
 
-  
-  def profile
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'users/_new'
+    end
   end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
