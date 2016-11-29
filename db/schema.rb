fb95b5ed83061783f3e97049a33d0441afccfa42
Ais93 <aismcauley00@gmail.com>
2016-11-29 04:14:55 +0000

    images dones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128151330) do
+ActiveRecord::Schema.define(version: 20161128211843) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(version: 20161128151330) do
     t.text     "description"
     t.text     "other"
     t.integer  "user_id"
-    t.integer  "category_id"
-    t.integer  "location_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "picture"
+    t.integer  "location_id"
+    t.integer  "category_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["location_id"], name: "index_posts_on_location_id"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
